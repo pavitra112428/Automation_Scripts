84835da97f7c6a97b3a35ee91bdc35e54c4e0f5a
unknown <pavitra112428@gmail.com>
2024-09-06 12:34:23 +0530

    changes3

--- src/test/java/day29.java
+++ src/test/java/day29.java
@@ -1,8 +1,18 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class day29 {
 	
 	public static void main(String[] args) {
-		System.out.println("pavi suppu");
-	}
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("");
+		driver.manage().window().maximize();
+		
 
+}
 }
